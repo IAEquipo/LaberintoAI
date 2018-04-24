eaa8ebaaedef3ba8330d864d073cc326e862f8ff
Alex <medinas.alex0001@gmail.com>
2018-04-24 03:46:13 -0500

    Anchura funcionando correctamente

--- src/main.py
+++ src/main.py
@@ -4,7 +4,8 @@
 # Módulos
 import pygame, sys, random, time
 from pygame.locals import *
-from anytree import Node, RenderTree, search
+from anytree import Node, RenderTree
+from anytree.search import *
 from anytree.dotexport import RenderTreeGraph
 
 #Modulos personales
@@ -58,6 +59,7 @@ beign = Beign('Human', posBeign[0], posBeign[1], costs)
 # ---------------------------------------------------------------------
 
 def anchura(nodo):
+    i = 0
     x = str(nodo).split("/")[-1].split(",")[0]
     y = str(nodo).split("/")[-1].split(",")[1].split("->")[0]
     beign.setX(int(x)*PIXEL)
@@ -65,9 +67,13 @@ def anchura(nodo):
     beign.setCostT(str(nodo).split("/")[-1].split(",")[1].split("->")[1].split("'")[0])
     padre = nodo
     back = False
-    forward = False
+    forward = True
     flagChild = False
+
+    #print("Nodo raiz de anchura: {}".format(padre))
+
     while(True):
+        #print("Iteracion: {}".format(i))
         if(pygame.mouse.get_pressed()[0] != 0):
             scene.ask_terrain(screen)
 
@@ -78,17 +84,26 @@ def anchura(nodo):
                 sys.exit(0)
         scene.paint_world(screen, matrix, 1)
 
+        #print(padre)
+
         if(pygame.mouse.get_pressed()[2] != 0):
             scene.change_terrain()
             scene.paint_world(screen, matrix, 1)
 
         if(back):
-            x = str(padre).split("/")[-1].split(",")[0]
-            y = str(padre).split("/")[-1].split(",")[1].split("->")[0]
-            beign.setX(int(x)*PIXEL)
-            beign.setY(int(y)*PIXEL)
-            beign.setCostT(str(padre).split("/")[-1].split(",")[1].split("->")[1].split("'")[0])
-            if(padre.is_root):
+            #print(padre)
+            flagChild = False
+            try:
+                x = str(padre).split("/")[-1].split(",")[0]
+                y = str(padre).split("/")[-1].split(",")[1].split("->")[0]
+                beign.setX(int(x)*PIXEL)
+                beign.setY(int(y)*PIXEL)
+                beign.setCostT(str(padre).split("/")[-1].split(",")[1].split("->")[1].split("'")[0])
+                #print(" Regresa x: {}\ty: {}".format(beign.getX//PIXEL, beign.getY//PIXEL))
+                forward = False
+            except AttributeError:
+                forward = True
+                back = False
                 return
 
         #print("1. {}".format(scene.askLEFT(beign.getX//PIXEL, beign.getY//PIXEL,0)))
@@ -98,18 +113,22 @@ def anchura(nodo):
         if(scene.askLEFT(beign.getX//PIXEL, beign.getY//PIXEL,0)):
             beign.LEFT(scene.askLEFT(beign.getX//PIXEL, beign.getY//PIXEL,0),1)
             back = False
+            forward = True
             flagChild = True
         elif(scene.askUP(beign.getX//PIXEL, beign.getY//PIXEL,0)):
             beign.UP(scene.askUP(beign.getX//PIXEL, beign.getY//PIXEL,0),1)
             back = False
+            forward = True
             flagChild = True
         elif(scene.askDOWN(beign.getX//PIXEL, beign.getY//PIXEL,0)):
             beign.DOWN(scene.askDOWN(beign.getX//PIXEL, beign.getY//PIXEL,0),1)
             back = False
+            forward = True
             flagChild = True
         elif(scene.askRIGHT(beign.getX//PIXEL, beign.getY//PIXEL,0)):
             beign.RIGHT(scene.askRIGHT(beign.getX//PIXEL, beign.getY//PIXEL,0),1)
             back = False
+            forward = True
             flagChild = True
         else:
             x = str(padre).split("/")[-1].split(",")[0]
@@ -124,12 +143,9 @@ def anchura(nodo):
                     forward = True
                     anchura(padre.children[i])
                 forward = False
+                back = True
             elif(padre.is_root):
-                x = str(padre).split("/")[-1].split(",")[0]
-                y = str(padre).split("/")[-1].split(",")[1].split("->")[0]
-                beign.setX(int(x)*PIXEL)
-                beign.setY(int(y)*PIXEL)
-                beign.setCostT(str(padre).split("/")[-1].split(",")[1].split("->")[1].split("'")[0])
+                return
             elif(back):
                 padre = padre.parent
                 x = str(padre).split("/")[-1].split(",")[0]
@@ -138,14 +154,20 @@ def anchura(nodo):
                 beign.setY(int(y)*PIXEL)
                 beign.setCostT(str(padre).split("/")[-1].split(",")[1].split("->")[1].split("'")[0])
                 forward = False
+                back = True
             else:
                 x = str(padre).split("/")[-1].split(",")[0]
                 y = str(padre).split("/")[-1].split(",")[1].split("->")[0]
                 beign.setX(int(x)*PIXEL)
                 beign.setY(int(y)*PIXEL)
                 back = True
+                forward = False
+
+        #print("x: {}\ty: {}".format(beign.getX//PIXEL, beign.getY//PIXEL))
 
         Decision = 0
+        #print("Decision: {}".format(Decision))
+
         #print("L: {}".format(scene.askLEFT(beign.getX//PIXEL, beign.getY//PIXEL,1)))
         if(scene.askLEFT(beign.getX//PIXEL, beign.getY//PIXEL,1)):
             Decision = Decision + 1
@@ -166,20 +188,41 @@ def anchura(nodo):
         Shadow = scene.getDarkSide()[beign.getY//PIXEL][beign.getX//PIXEL][0]
 
         #print("Decision: {}".format(Decision))
+        #print("is_root: {}".format(padre.is_root))
         if(Decision > 2):
             d = "d"
             if (flagChild == True):
                 flagChild = False
-                hijo = Node("" + str(beign.getX//PIXEL) + "," + str(beign.getY//PIXEL) + "->" + str(beign.getCostT) + "", parent=padre)
-                padre  = hijo.parent
+                #print("find: {}".format(findall(padre, lambda node: node.name == "" + str(beign.getX//PIXEL) + "," + str(beign.getY//PIXEL) + "->" + str(beign.getCostT) + "")))
+                if (findall(padre, lambda node: node.name == "" + str(beign.getX//PIXEL) + "," + str(beign.getY//PIXEL) + "->" + str(beign.getCostT) + "") != None):
+                    #print("D2 -> 1:{}".format(padre))
+                    aux = str(padre).split("/")[-1].split("->")[0]
+                    #print("aux: {}".format(aux))
+                    #print("cad: {}".format("" + str(beign.getX//PIXEL) + "," + str(beign.getY//PIXEL) + ""))
+                    if(("" + str(beign.getX//PIXEL) + "," + str(beign.getY//PIXEL) + "") != aux):
+                        padre = Node("" + str(beign.getX//PIXEL) + "," + str(beign.getY//PIXEL) + "->" + str(beign.getCostT) + "", parent=padre)
+                        padre  = padre.parent
+                        #print("D2 -> 2:{}".format(padre))
                 back = True
+                forward = False
                 #print(RenderTree(raiz))
-        elif(Decision == 1):
+        elif(Decision == 1 and not padre.is_root):
             d = 0
-            hijo = Node("" + str(beign.getX//PIXEL) + "," + str(beign.getY//PIXEL) + "->" + str(beign.getCostT) + "", parent=padre)
-            padre = hijo.parent
-            back = True
-            print(RenderTree(nodo))
+            #print("find: {}".format(findall(padre, lambda node: node.name == "" + str(beign.getX//PIXEL) + "," + str(beign.getY//PIXEL) + "->" + str(beign.getCostT) + "")))
+            if (flagChild == True):
+                flagChild = False
+                if (findall(padre, lambda node: node.name == "" + str(beign.getX//PIXEL) + "," + str(beign.getY//PIXEL) + "->" + str(beign.getCostT) + "") != None):
+                    #print("D1 -> 1:{}".format(padre))
+                    aux = str(padre).split("/")[-1].split("->")[0]
+                    #print("aux: {}".format(aux))
+                    #print("cad: {}".format("" + str(beign.getX//PIXEL) + "," + str(beign.getY//PIXEL) + ""))
+                    if(("" + str(beign.getX//PIXEL) + "," + str(beign.getY//PIXEL) + "") != aux):
+                        padre = Node("" + str(beign.getX//PIXEL) + "," + str(beign.getY//PIXEL) + "->" + str(beign.getCostT) + "", parent=padre)
+                        padre = padre.parent
+                        #print("D1 -> 2:{}".format(padre))
+                back = True
+                forward = False
+                #print(RenderTree(nodo))
         else:
             d = 0
 
@@ -195,24 +238,26 @@ def anchura(nodo):
         string = "{0}"
         if (etiqueta[0] <= scene.getDimensions()[0] and etiqueta[1] <= scene.getDimensions()[1]):
             scene.displayInfo(screen, string.format(scene.getDarkSide()[etiqueta[1]//PIXEL][etiqueta[0]//PIXEL]))
+        i += 1
         pygame.display.flip()
-        reloj.tick(1)
-
+        reloj.tick(10)
 
 
 
 def main():
     back = False
+    forward = True
+    #i = 0
     raiz = Node("" + str(beign.getX//PIXEL) + "," + str(beign.getY//PIXEL) + "->0")
     padre = raiz
     scene.getDarkSide()[beign.getY//PIXEL][beign.getX//PIXEL][3] ="d"
+    #Temp = 0
 
-    Temp = 0
-    forward = True
     scene.paint_world(screen, scene.getDarkSide(), 0)
     scene.paint_beign(screen, beign.getX, beign.getY)
 
     while True:
+        #print(i)
         if(pygame.mouse.get_pressed()[0] != 0):
             scene.ask_terrain(screen)
 
@@ -228,11 +273,14 @@ def main():
             scene.paint_world(screen, matrix, 1)
 
         if(back):
+            #print(padre)
             x = str(padre).split("/")[-1].split(",")[0]
             y = str(padre).split("/")[-1].split(",")[1].split("->")[0]
             beign.setX(int(x)*PIXEL)
             beign.setY(int(y)*PIXEL)
             beign.setCostT(str(padre).split("/")[-1].split(",")[1].split("->")[1].split("'")[0])
+            flagChild = False
+            #print(" Regresa x: {}\ty: {}".format(beign.getX//PIXEL, beign.getY//PIXEL))
 
         #print("1. {}".format(scene.askLEFT(beign.getX//PIXEL, beign.getY//PIXEL,0)))
         #print("2. {}".format(scene.askUP(beign.getX//PIXEL, beign.getY//PIXEL,0)))
@@ -262,11 +310,12 @@ def main():
             beign.setCostT(str(padre).split("/")[-1].split(",")[1].split("->")[1].split("'")[0])
 
             if(padre.is_root):
-                print(padre.children)
+                #print(padre.children)
                 for i in range(len(padre.children)):
-                    print("for de main: {}".format(i))
+                    #print("for de main: {}".format(i))
                     forward = True
                     anchura(padre.children[i])
+                print(RenderTree(raiz))
                 forward = False
             elif(back):
                 padre = padre.parent
@@ -283,7 +332,9 @@ def main():
                 beign.setY(int(y)*PIXEL)
                 back = True
 
+        #print("x: {}\ty: {}".format(beign.getX//PIXEL, beign.getY//PIXEL))
         Decision = 0
+        #print("Decision: {}".format(Decision))
         #print("L: {}".format(scene.askLEFT(beign.getX//PIXEL, beign.getY//PIXEL,1)))
         if(scene.askLEFT(beign.getX//PIXEL, beign.getY//PIXEL,1)):
             Decision = Decision + 1
@@ -317,7 +368,7 @@ def main():
             hijo = Node("" + str(beign.getX//PIXEL) + "," + str(beign.getY//PIXEL) + "->" + str(beign.getCostT) + "", parent=padre)
             padre = hijo.parent
             back = True
-            print(RenderTree(raiz))
+            #print(RenderTree(raiz))
         else:
             d = 0
 
@@ -333,8 +384,9 @@ def main():
         string = "{0}"
         if (etiqueta[0] <= scene.getDimensions()[0] and etiqueta[1] <= scene.getDimensions()[1]):
             scene.displayInfo(screen, string.format(scene.getDarkSide()[etiqueta[1]//PIXEL][etiqueta[0]//PIXEL]))
+        #i += 1
         pygame.display.flip()
-        reloj.tick(1)
+        reloj.tick(10)
 
 
 if __name__ == '__main__':
